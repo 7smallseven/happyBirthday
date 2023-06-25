797b7629d37dfb69604b1748d847d4c377e5b71b
PunkCat <37116488+7smallseven@users.noreply.github.com>
2023-06-25 22:09:24 +0800

    初始化

---
+++ src/main/java/com/punkcat/happybirthday/HappyBirthdayApplication.java
@@ -0,0 +1,16 @@
+package com.punkcat.happybirthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author seven
+ */
+@SpringBootApplication
+public class HappyBirthdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyBirthdayApplication.class, args);
+    }
+
+}
